d5a20eccde0870c8089b573ac3a8cd08d49babfb
Tony Brown <anthonyjaybrown@gmail.com>
2024-11-10 21:33:56 -0700

    firebase init with util no apikey

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -5,10 +5,13 @@ import 'firebase/auth';
 // TODO Config this
 const config = {
   apiKey: "",
-  authDomain: "fir-v2.firebaseapp.com",
-  databaseURL: "https://fir-v2.firebaseio.com",
-  storageBucket: "fir-v2.appspot.com",
-  messagingSenderId: ""
+  authDomain: "discdrafter.firebaseapp.com",
+  databaseURL: "https://discdrafter-default-rtdb.firebaseio.com",
+  projectId: "discdrafter",
+  storageBucket: "discdrafter.firebasestorage.app",
+  messagingSenderId: "690492423129",
+  appId: "1:690492423129:web:4dff9b10113da33a2a37c8",
+  measurementId: "G-HZKKPBRKF5"
 };
 const app = firebase.initializeApp(config);
 
